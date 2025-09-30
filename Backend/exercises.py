365017d1fdb3921b303661bf8d6ed661fdbd90a4
kucupwn <kucukasd@gmail.com>
2025-09-30 18:31:22 +0200

    add exercises router

--- Backend/exercises.py
+++ Backend/exercises.py
@@ -1,7 +1,9 @@
 from typing import Annotated
 from sqlalchemy.orm import Session
 from fastapi import Depends, APIRouter, HTTPException
+from starlette import status
 from .database import SessionLocal
+from .models import Exercises
 
 router = APIRouter(prefix="/exercises", tags=["exercises"])
 
@@ -15,3 +17,8 @@ def get_db():
 
 
 db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@router.get("/", status_code=status.HTTP_200_OK)
+async def read_all(db: db_dependency):
+    return db.query(Exercises).all()
--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from .models import Base
 from .database import engine
+from . import exercises
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
+
+app.include_router(exercises.router)
