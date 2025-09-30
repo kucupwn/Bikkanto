560cfad6aff6da6ffc07018b2ad559bae80ebfb8
kucupwn <kucukasd@gmail.com>
2025-09-30 17:45:11 +0200

    create models and db

---
+++ models.py
@@ -0,0 +1,16 @@
+from database import Base
+from sqlalchemy import Column, Integer, String
+
+
+class Exercises(Base):
+    __tablename__ = "exercises"
+
+    id = Column(Integer, primary_key=True, index=True)
+    exercise_name = Column(String, unique=True)
+    category = Column(String)
+    easy_min = Column(Integer)
+    easy_max = Column(Integer)
+    medium_min = Column(Integer)
+    medium_max = Column(Integer)
+    hard_min = Column(Integer)
+    hard_max = Column(Integer)
