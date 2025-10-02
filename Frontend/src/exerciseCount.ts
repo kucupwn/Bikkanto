defff08473306c15bd9e73e87a3378d83e364e9c
kucupwn <kucukasd@gmail.com>
2025-10-02 17:22:41 +0200

    format with prettier

--- Frontend/src/exerciseCount.ts
+++ Frontend/src/exerciseCount.ts
@@ -1,17 +1,23 @@
-function fillExerciseCount(selectId: string, start: number, stop: number): void {
-    const exerciseCount = document.getElementById(selectId) as HTMLSelectElement | null;
-    if (!exerciseCount) return
+function fillExerciseCount(
+  selectId: string,
+  start: number,
+  stop: number
+): void {
+  const exerciseCount = document.getElementById(
+    selectId
+  ) as HTMLSelectElement | null;
+  if (!exerciseCount) return;
 
-    exerciseCount.innerHTML = ''
+  exerciseCount.innerHTML = "";
 
-    for (let i=start; i<=stop; i++) {
-        let option = document.createElement('option');
-        option.value = String(i);
-        option.textContent = String(i);
-        exerciseCount.appendChild(option)
-    }
+  for (let i = start; i <= stop; i++) {
+    let option = document.createElement("option");
+    option.value = String(i);
+    option.textContent = String(i);
+    exerciseCount.appendChild(option);
+  }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    fillExerciseCount('exercise-count', 1, 20)
-})
+document.addEventListener("DOMContentLoaded", () => {
+  fillExerciseCount("exercise-count", 1, 20);
+});
