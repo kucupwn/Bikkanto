e3edb7a7a8883237f5df93e1096456925347eb88
kucupwn <kucukasd@gmail.com>
2025-10-04 12:18:58 +0200

    temporary fix for cors problem

--- Backend/main.py
+++ Backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .models import Base
 from .database import engine
 from . import exercises
@@ -8,3 +9,16 @@ app = FastAPI()
 Base.metadata.create_all(bind=engine)
 
 app.include_router(exercises.router)
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
