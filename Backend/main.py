515ae72b5be86732af707b3e0a31352a9d14179f
kucupwn <kucukasd@gmail.com>
2025-10-07 07:45:10 +0200

    use backend cors origin from config

--- Backend/main.py
+++ Backend/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from .models import Base
 from .database import engine
 from .routers import exercises
+from .core.config import settings
 
 app = FastAPI()
 
@@ -10,14 +11,10 @@ Base.metadata.create_all(bind=engine)
 
 app.include_router(exercises.router)
 
-origins = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=settings.BACKEND_CORS_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
