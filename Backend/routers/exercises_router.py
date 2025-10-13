6adb967e923cf58b933ada7fa7ebcfa8bcb52016
kucupwn <kucukasd@gmail.com>
2025-10-13 18:30:36 +0200

    add delete

--- Backend/routers/exercises_router.py
+++ Backend/routers/exercises_router.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 from sqlalchemy.orm import Session
-from fastapi import Depends, APIRouter, HTTPException
+from fastapi import Depends, APIRouter, HTTPException, Path
 from starlette import status
 from ..database import SessionLocal
 from ..models import Exercises
@@ -25,9 +25,7 @@ async def read_all(db: db_dependency):
     return db.query(Exercises).all()
 
 
-@router.post(
-    "/exercise", response_model=ExercisesRead, status_code=status.HTTP_201_CREATED
-)
+@router.post("/", response_model=ExercisesRead, status_code=status.HTTP_201_CREATED)
 async def create_exercise(db: db_dependency, exercise_create: ExerciseCreate):
     existing = (
         db.query(Exercises)
@@ -35,7 +33,7 @@ async def create_exercise(db: db_dependency, exercise_create: ExerciseCreate):
         .first()
     )
     if existing:
-        raise HTTPException(status_code=400, detail="Exercise already exists.")
+        raise HTTPException(status_code=400, detail="Exercise already exists")
 
     exercise_model = Exercises(**exercise_create.model_dump())
     db.add(exercise_model)
@@ -43,3 +41,18 @@ async def create_exercise(db: db_dependency, exercise_create: ExerciseCreate):
     db.refresh(exercise_model)
 
     return exercise_model
+
+
+@router.delete(
+    "/{exercise_id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+)
+async def delete_exercise(db: db_dependency, exercise_id: int = Path(gt=0)):
+    exercise_model = db.query(Exercises).filter(Exercises.id == exercise_id).first()
+
+    if not exercise_model:
+        raise HTTPException(status_code=404, detail="Exercise not found")
+
+    db.query(Exercises).filter(Exercises.id == exercise_id).delete()
+
+    db.commit()
