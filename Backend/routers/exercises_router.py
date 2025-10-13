d428bc44f496f1339fe4d439c99b4d6d597f62a8
kucupwn <kucukasd@gmail.com>
2025-10-13 19:04:54 +0200

    add patch method

--- Backend/routers/exercises_router.py
+++ Backend/routers/exercises_router.py
@@ -4,7 +4,7 @@ from fastapi import Depends, APIRouter, HTTPException, Path
 from starlette import status
 from ..database import SessionLocal
 from ..models import Exercises
-from ..schemas import ExercisesRead, ExerciseCreate
+from ..schemas import ExercisesRead, ExerciseCreate, ExercisesUpdate
 
 router = APIRouter(prefix="/exercises", tags=["exercises"])
 
@@ -37,6 +37,28 @@ async def create_exercise(db: db_dependency, exercise_create: ExerciseCreate):
 
     exercise_model = Exercises(**exercise_create.model_dump())
     db.add(exercise_model)
+    db.commit()
+    db.refresh(exercise_model)
+
+    return exercise_model
+
+
+@router.patch(
+    "/{exercise_id}", response_model=ExercisesRead, status_code=status.HTTP_200_OK
+)
+async def update_exercise(
+    db: db_dependency, exercise_update: ExercisesUpdate, exercise_id: int = Path(gt=0)
+):
+    exercise_model = db.query(Exercises).filter(Exercises.id == exercise_id).first()
+
+    if exercise_model is None:
+        raise HTTPException(status_code=404, detail="Exercise not found")
+
+    update_data = exercise_update.model_dump(exclude_unset=True)
+
+    for key, value in update_data.items():
+        setattr(exercise_model, key, value)
+
     db.commit()
     db.refresh(exercise_model)
 
