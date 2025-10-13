f43e77ef08e2ae2769280bba166ee8478236fac3
kucupwn <kucukasd@gmail.com>
2025-10-13 18:07:40 +0200

    use exercise create schema

--- Backend/routers/exercises_router.py
+++ Backend/routers/exercises_router.py
@@ -2,9 +2,9 @@ from typing import Annotated
 from sqlalchemy.orm import Session
 from fastapi import Depends, APIRouter, HTTPException
 from starlette import status
-from pydantic import BaseModel, Field
 from ..database import SessionLocal
 from ..models import Exercises
+from ..schemas import ExercisesRead, ExerciseCreate
 
 router = APIRouter(prefix="/exercises", tags=["exercises"])
 
@@ -20,24 +20,18 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
-class ExerciseRequest(BaseModel):
-    exercise_name: str = Field(min_length=1)
-    category: str = Field(min_length=1)
-    easy_min: int = Field(gt=0)
-    easy_max: int = Field(gt=0)
-    medium_min: int = Field(gt=0)
-    medium_max: int = Field(gt=0)
-    hard_min: int = Field(gt=0)
-    hard_max: int = Field(gt=0)
-
-
-@router.get("/", status_code=status.HTTP_200_OK)
+@router.get("/", response_model=list[ExercisesRead], status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependency):
     return db.query(Exercises).all()
 
 
-@router.post("/exercise", status_code=status.HTTP_201_CREATED)
-async def create_exercise(db: db_dependency, exercise_request: ExerciseRequest):
-    exercise_model = Exercises(**exercise_request.model_dump())
+@router.post(
+    "/exercise", response_model=ExercisesRead, status_code=status.HTTP_201_CREATED
+)
+async def create_exercise(db: db_dependency, exercise_create: ExerciseCreate):
+    exercise_model = Exercises(**exercise_create.model_dump())
     db.add(exercise_model)
     db.commit()
+    db.refresh(exercise_model)
+
+    return exercise_model
