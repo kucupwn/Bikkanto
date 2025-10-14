c6b52a9a9e4d5c822455c6e401d09653aece5547
Bence Gombkötő <kucukasd@gmail.com>
2025-10-14 18:23:21 +0200

    create users model

--- Backend/models.py
+++ Backend/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean
 from .database import Base
 
 
@@ -14,3 +14,16 @@ class Exercises(Base):
     medium_max = Column(Integer)
     hard_min = Column(Integer)
     hard_max = Column(Integer)
+
+
+class Users(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
