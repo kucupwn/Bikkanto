8dce83e79a0665a95769a9238e468a2c06b365ab
Bence Gombkötő <kucukasd@gmail.com>
2025-10-15 17:17:23 +0200

    include routers to main

--- Backend/main.py
+++ Backend/main.py
@@ -2,14 +2,16 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .models import Base
 from .database import engine
-from .routers import exercises_router
+from .routers import exercises_router, auth, users_router
 from .core.config import settings
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
+app.include_router(auth.router)
 app.include_router(exercises_router.router)
+app.include_router(users_router.router)
 
 
 app.add_middleware(
