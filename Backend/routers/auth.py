0f100b248af06a39a7ef19684ff7f85206b3f4a1
Bence Gombkötő <kucukasd@gmail.com>
2025-10-15 17:21:00 +0200

    remove basemodel import from auth

--- Backend/routers/auth.py
+++ Backend/routers/auth.py
@@ -2,7 +2,6 @@ from datetime import datetime, timedelta, timezone
 from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
-from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from starlette import status
 from passlib.context import CryptContext
