9d119ece3f82e7f236f7b743750dc6eced44204c
Bence Gombkötő <kucukasd@gmail.com>
2025-10-15 16:40:40 +0200

    add user read and create to user router

--- Backend/routers/users.py
+++ Backend/routers/users.py
@@ -1,9 +1,11 @@
 from typing import Annotated
 from sqlalchemy.orm import Session
-from fastapi import Depends, APIRouter, HTTPException, Path
+from fastapi import Depends, APIRouter, HTTPException
 from starlette import status
 from passlib.context import CryptContext
 from ..database import get_db
+from ..models import Users
+from ..schemas.users_schema import UserRead, UserCreate, UserUpdate
 from .auth import get_current_user
 
 router = APIRouter(prefix="/users", tags=["users"])
@@ -13,3 +15,28 @@ bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[dict, Depends(get_current_user)]
+
+
+@router.get("/", response_model=UserRead, status_code=status.HTTP_200_OK)
+async def get_user(user: user_dependency, db: db_dependency):
+    if user is None:
+        raise HTTPException(status_code=401, detail="Authentication Failed")
+
+    user_model = db.query(Users).filter(Users.id == user.get("id")).first()
+
+    return UserRead.model_validate(user_model)
+
+
+@router.post("/", response_model=UserRead, status_code=status.HTTP_201_CREATED)
+async def create_user(create_user_request: UserCreate, db: db_dependency):
+    user_data = create_user_request.model_dump(exclude={"password"})
+    create_user_model = Users(
+        **user_data,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
+        is_active=True
+    )
+    db.add(create_user_model)
+    db.commit()
+    db.refresh(create_user_model)
+
+    return UserRead.model_validate(create_user_model)
