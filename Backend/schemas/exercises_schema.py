dc32d3dea998d71b55ff4d0191705d22c7235dff
Bence Gombkötő <kucukasd@gmail.com>
2025-10-20 13:59:15 +0200

    add category enum to exc schema

--- Backend/schemas/exercises_schema.py
+++ Backend/schemas/exercises_schema.py
@@ -1,9 +1,21 @@
+from enum import Enum
 from pydantic import BaseModel, Field
+
+
+class Category(str, Enum):
+    PUSH_UP = "push up"
+    PULL_UP = "pull up"
+    ABS = "abs"
+    LOWER_BACK = "lower back"
+    PLANCHE = "planche"
+    FRONT_LEVER = "front lever"
+    REHAB = "rehab"
+    OTHER = "other"
 
 
 class ExerciseBase(BaseModel):
     exercise_name: str
-    category: str
+    category: Category
     easy_min: int | None = None
     easy_max: int | None = None
     medium_min: int | None = None
@@ -21,7 +33,7 @@ class ExerciseRead(ExerciseBase):
 
 class ExerciseCreate(ExerciseBase):
     exercise_name: str = Field(..., min_length=1)
-    category: str = Field(..., min_length=1)
+    category: Category = Field(...)
     easy_min: int | None = Field(None, gt=0)
     easy_max: int | None = Field(None, gt=0)
     medium_min: int | None = Field(None, gt=0)
@@ -32,7 +44,7 @@ class ExerciseCreate(ExerciseBase):
 
 class ExerciseUpdate(BaseModel):
     exercise_name: str | None = Field(None, min_length=1)
-    category: str | None = Field(None, min_length=1)
+    category: Category | None = Field(None, min_length=1)
     easy_min: int | None = Field(None, gt=0)
     easy_max: int | None = Field(None, gt=0)
     medium_min: int | None = Field(None, gt=0)
