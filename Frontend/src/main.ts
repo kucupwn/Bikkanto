1002c1bac9ff4475a0110dc4af584e44ace45b95
Bence Gombkötő <kucukasd@gmail.com>
2025-10-24 13:27:12 +0200

    import bootstrap module to make mobile navbar work

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -1,4 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { header, loadPartial, setActiveNav } from "../partials/header";
 
 window.addEventListener("load", async () => {
