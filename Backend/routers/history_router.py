274fb8046700ef1ad6e843b60d3de3aa3c838d22
Bence Gombkötő <kucukasd@gmail.com>
2025-10-26 08:18:04 +0100

    add history endpoints

--- Backend/routers/history_router.py
+++ Backend/routers/history_router.py
@@ -9,3 +9,18 @@ from ..schemas.history_schema import HistoryRead, HistoryCreate
 router = APIRouter(prefix="/history", tags=["history"])
 
 db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@router.get("/", response_model=list[HistoryRead], status_code=status.HTTP_200_OK)
+async def read_all(db: db_dependency):
+    return db.query(History).all()
+
+
+@router.post("/", response_model=HistoryRead, status_code=status.HTTP_201_CREATED)
+async def create_history_entry(db: db_dependency, history_create: HistoryCreate):
+    history_model = History(**history_create.model_dump())
+    db.add(history_model)
+    db.commit()
+    db.refresh(history_model)
+
+    return history_model
