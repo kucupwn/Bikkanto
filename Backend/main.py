4477066d47a3d765725410ebb88183e396f412c9
Bence Gombkötő <kucukasd@gmail.com>
2025-10-27 17:42:39 +0100

    add history router to main py

--- Backend/main.py
+++ Backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .models import Base
 from .database import engine
-from .routers import exercises_router, auth, users_router
+from .routers import exercises_router, auth, users_router, history_router
 from .core.config import settings
 
 app = FastAPI()
@@ -12,6 +12,7 @@ Base.metadata.create_all(bind=engine)
 app.include_router(auth.router)
 app.include_router(exercises_router.router)
 app.include_router(users_router.router)
+app.include_router(history_router.router)
 
 
 app.add_middleware(
