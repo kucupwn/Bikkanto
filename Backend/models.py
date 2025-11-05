a630d630ae15b9e192830e427d13e74b9e64ae7a
Bence Gombkötő <kucukasd@gmail.com>
2025-11-05 14:40:39 +0100

    remove role and is_active from users model

--- Backend/models.py
+++ Backend/models.py
@@ -33,9 +33,7 @@ class Users(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True)
-    role = Column(String)
     email = Column(String, unique=True)
     first_name = Column(String)
     last_name = Column(String)
     hashed_password = Column(String)
-    is_active = Column(Boolean, default=True)
