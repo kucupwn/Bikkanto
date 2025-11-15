6a09048315e93142079888c0be9491788862cdd8
Bence Gombkötő <kucukasd@gmail.com>
2025-11-15 09:14:17 +0100

    return bool if page protected

--- Frontend/src/ui/protected.ts
+++ Frontend/src/ui/protected.ts
@@ -1,8 +1,11 @@
 import { users } from "../users/users";
 
-export function protectPage() {
+export function protectPage(): boolean {
   const user = users.getCurrentUser();
   if (!user) {
     window.location.href = "/login.html";
+    return true;
+  } else {
+    return false;
   }
 }
