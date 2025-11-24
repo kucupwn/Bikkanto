cda44bfe3a0979f3e644688f9d22759b46cc26e8
Bence Gombkötő <kucukasd@gmail.com>
2025-11-24 14:11:37 +0100

    install and add gear icon as settings

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -1,5 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 import { header } from "./partials/header";
 import { users } from "./users/users";
 import { protectPage } from "./ui/protected";
