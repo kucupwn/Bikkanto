22245e03bd98ed7a8e52041b95d64a3723e20307
Bence Gombkötő <kucukasd@gmail.com>
2025-11-26 13:57:34 +0100

    reorder users functions

--- Frontend/src/users/users.ts
+++ Frontend/src/users/users.ts
@@ -62,6 +62,146 @@ export class Users {
       });
   }
 
+  // --------------------------------------------------------------
+  // LOGIN/LOGOUT
+  // --------------------------------------------------------------
+
+  public async login(username: string, password: string): Promise<void> {
+    const formData = new URLSearchParams();
+    formData.append("username", username);
+    formData.append("password", password);
+
+    await this.getToken(formData);
+
+    window.location.href = "/index.html";
+  }
+
+  private async getToken(formData: URLSearchParams): Promise<void> {
+    const res = await fetch(`${this.apiUrl}/auth/token`, {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: formData,
+    });
+
+    if (!res.ok) {
+      alert("Invalid username or password!");
+      return;
+    }
+
+    const data = await res.json();
+    const token = data.access_token;
+    localStorage.setItem("token", token);
+
+    this.startAutoLogout(token);
+  }
+
+  public logout(): void {
+    localStorage.removeItem("token");
+    window.location.href = "/index.html";
+  }
+
+  private startAutoLogout(token: string): void {
+    const payload = JSON.parse(atob(token.split(".")[1]));
+
+    const expiry = payload.exp * 1000;
+    const now = Date.now();
+    const timeLeft = expiry - now;
+
+    if (timeLeft <= 0) {
+      this.logout();
+      return;
+    }
+
+    setTimeout(() => {
+      this.logout();
+    }, timeLeft);
+  }
+
+  private async addNewUser(formData: HTMLFormElement): Promise<void> {
+    const data = getNewUserFormData(formData);
+    if (!data) return;
+
+    try {
+      const res = await fetch(`${this.apiUrl}/users/register`, {
+        method: "POST",
+        headers: { "Content-Type": "application/JSON" },
+        body: JSON.stringify(data),
+      });
+
+      if (!res.ok) {
+        const err = await res.json();
+        console.error("Backend error response:", err);
+        throw new Error(JSON.stringify(err));
+      }
+
+      window.location.href = "/index.html";
+    } catch (err) {
+      console.warn("Error creating user: ", err);
+    }
+  }
+
+  // --------------------------------------------------------------
+  // GET USER DATA
+  // --------------------------------------------------------------
+
+  public getCurrentUser(): AuthUser | null {
+    const token = localStorage.getItem("token");
+    if (!token) return null;
+
+    const payload = JSON.parse(atob(token.split(".")[1]));
+
+    return {
+      id: payload.id,
+      username: payload.sub,
+      role: payload.role,
+    };
+  }
+
+  private async getCurrentUserAllDetails(
+    currentUser: AuthUser
+  ): Promise<User | null> {
+    if (!currentUser) {
+      return null;
+    }
+
+    const token = localStorage.getItem("token");
+
+    try {
+      const res = await fetch(`${this.apiUrl}/users`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const data: User = await res.json();
+
+      return data;
+    } catch (err) {
+      console.warn("Error loading data: ", err);
+
+      return null;
+    }
+  }
+
+  // --------------------------------------------------------------
+  // USER DATA EDITING
+  // --------------------------------------------------------------
+
+  public async fillSettingsPageWithCurrentData(
+    currentUser: AuthUser
+  ): Promise<void> {
+    const userData = await this.getCurrentUserAllDetails(currentUser);
+    const firstName = document.getElementById("firstname-paragraph");
+    const lastName = document.getElementById("lastname-paragraph");
+    const email = document.getElementById("email-paragraph");
+
+    setCurrentSettingsTextContent(firstName, userData?.first_name);
+    setCurrentSettingsTextContent(lastName, userData?.last_name);
+    setCurrentSettingsTextContent(email, userData?.email);
+  }
+
   private openEditModal(
     editKey: string,
     headerLabel: string,
@@ -129,134 +269,6 @@ export class Users {
       window.location.reload();
     } catch (err) {
       console.error("Could not update user data:", err);
-    }
-  }
-
-  public async login(username: string, password: string): Promise<void> {
-    const formData = new URLSearchParams();
-    formData.append("username", username);
-    formData.append("password", password);
-
-    await this.getToken(formData);
-
-    window.location.href = "/index.html";
-  }
-
-  public logout(): void {
-    localStorage.removeItem("token");
-    window.location.href = "/index.html";
-  }
-
-  private async getToken(formData: URLSearchParams): Promise<void> {
-    const res = await fetch(`${this.apiUrl}/auth/token`, {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: formData,
-    });
-
-    if (!res.ok) {
-      alert("Invalid username or password!");
-      return;
-    }
-
-    const data = await res.json();
-    const token = data.access_token;
-    localStorage.setItem("token", token);
-
-    this.startAutoLogout(token);
-  }
-
-  public getCurrentUser(): AuthUser | null {
-    const token = localStorage.getItem("token");
-    if (!token) return null;
-
-    const payload = JSON.parse(atob(token.split(".")[1]));
-
-    return {
-      id: payload.id,
-      username: payload.sub,
-      role: payload.role,
-    };
-  }
-
-  private async addNewUser(formData: HTMLFormElement): Promise<void> {
-    const data = getNewUserFormData(formData);
-    if (!data) return;
-
-    try {
-      const res = await fetch(`${this.apiUrl}/users/register`, {
-        method: "POST",
-        headers: { "Content-Type": "application/JSON" },
-        body: JSON.stringify(data),
-      });
-
-      if (!res.ok) {
-        const err = await res.json();
-        console.error("Backend error response:", err);
-        throw new Error(JSON.stringify(err));
-      }
-
-      window.location.href = "/index.html";
-    } catch (err) {
-      console.warn("Error creating user: ", err);
-    }
-  }
-
-  private startAutoLogout(token: string): void {
-    const payload = JSON.parse(atob(token.split(".")[1]));
-
-    const expiry = payload.exp * 1000;
-    const now = Date.now();
-    const timeLeft = expiry - now;
-
-    if (timeLeft <= 0) {
-      this.logout();
-      return;
-    }
-
-    setTimeout(() => {
-      this.logout();
-    }, timeLeft);
-  }
-
-  public async fillSettingsPageWithCurrentData(
-    currentUser: AuthUser
-  ): Promise<void> {
-    const userData = await this.getCurrentUserAllDetails(currentUser);
-    const firstName = document.getElementById("firstname-paragraph");
-    const lastName = document.getElementById("lastname-paragraph");
-    const email = document.getElementById("email-paragraph");
-
-    setCurrentSettingsTextContent(firstName, userData?.first_name);
-    setCurrentSettingsTextContent(lastName, userData?.last_name);
-    setCurrentSettingsTextContent(email, userData?.email);
-  }
-
-  private async getCurrentUserAllDetails(
-    currentUser: AuthUser
-  ): Promise<User | null> {
-    if (!currentUser) {
-      return null;
-    }
-
-    const token = localStorage.getItem("token");
-
-    try {
-      const res = await fetch(`${this.apiUrl}/users`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      });
-
-      const data: User = await res.json();
-
-      return data;
-    } catch (err) {
-      console.warn("Error loading data: ", err);
-
-      return null;
     }
   }
 }
