a3f0d13693cc29503e59582d66ea74a49941178a
Bence Gombkötő <kucukasd@gmail.com>
2025-11-27 16:41:30 +0100

    add roll callbacks

---
+++ Frontend/src/types/roll.types.ts
@@ -0,0 +1,13 @@
+import type { WorkoutEntry } from "./exercises.types";
+
+export type RollCallbacks = {
+  onGetExerciseSelections: (exerciseCount: number) => void;
+  onGetWorkout: () => WorkoutEntry[];
+  onFillOverviewTable: (workout: WorkoutEntry[]) => void;
+  onApplyRoll: () => void;
+  onToggleRollSettingsOverview: () => void;
+  onToggleRollOverviewSubmit: () => void;
+  onSaveWorkoutHistory: () => Promise<void> | void;
+  onToggleUnsubmittedRollDisplay: () => void;
+  onLoadUnsubmittedTable: () => void;
+};
