f945e42cb153a5c0387297fffea2872b85c9fe22
Bence Gombkötő <kucukasd@gmail.com>
2025-12-01 01:41:12 +0100

    change load to DOMContentLoaded in main.ts

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -1,5 +1,4 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 import { header } from "./partials/header";
@@ -8,18 +7,18 @@ import { protectPage } from "./ui/protected";
 import { setHomeUi } from "./ui/home";
 import type { AuthUser } from "./types/user.types";
 
-window.addEventListener("load", async () => {
+window.addEventListener("DOMContentLoaded", async () => {
   const currentUser = users.getCurrentUser() as AuthUser;
   protectPage(currentUser);
 
   await header.init();
   header.toggleLoginButton(currentUser);
 
-  if (window.location.pathname === "/index.html") {
+  if (window.location.pathname.endsWith("index.html")) {
     setHomeUi(currentUser?.username || null);
   }
 
-  if (window.location.pathname === "/profile.html") {
+  if (window.location.pathname.endsWith("profile.html")) {
     users.fillProfilePageWithCurrentData(currentUser);
   }
 
