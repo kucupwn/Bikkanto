a93fb6add59cb46e1bb0835e644657d7b42147d5
Bence Gombkötő <kucukasd@gmail.com>
2025-12-05 10:39:12 +0100

    add change password endpoint

--- Backend/routers/users_router.py
+++ Backend/routers/users_router.py
@@ -5,7 +5,7 @@ from starlette import status
 from passlib.context import CryptContext
 from ..database import get_db
 from ..models import Users
-from ..schemas.users_schema import UserRead, UserCreate, UserUpdate
+from ..schemas.users_schema import UserRead, UserCreate, UserUpdate, UserVerification
 from .auth import get_current_user
 
 router = APIRouter(prefix="/users", tags=["users"])
@@ -105,3 +105,22 @@ async def update_user(
     db.refresh(user_model)
 
     return UserRead.model_validate(user_model)
+
+
+@router.put("/change_password", status_code=status.HTTP_204_NO_CONTENT)
+async def change_password(
+    user: user_dependency, db: db_dependency, user_verification: UserVerification
+):
+    if user is None:
+        raise HTTPException(status_code=401, detail="Authentication Failed")
+
+    user_model = db.query(Users).filter(Users.id == user.get("id")).first()
+
+    if not bcrypt_context.verify(
+        user_verification.password, user_model.hashed_password
+    ):
+        raise HTTPException(status_code=401, detail="Error on password change")
+
+    user_model.hashed_password = bcrypt_context.hash(user_verification.password)
+
+    db.commit()
