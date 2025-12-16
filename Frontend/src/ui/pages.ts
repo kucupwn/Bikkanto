d46b3d90c63c43e856b07179a94ae0b833798bae
Bence Gombkötő <kucukasd@gmail.com>
2025-12-16 14:44:04 +0100

    add pages.ts, move protectPage and setHomeUi here

--- Frontend/src/ui/pages.ts
+++ Frontend/src/ui/pages.ts
@@ -1,3 +1,22 @@
+import type { AuthUser } from "../types/user.types";
+
+export function protectPage(user: AuthUser | null): boolean {
+  const excludedPages = ["/index.html", "/login.html", "/register.html"];
+
+  const path = window.location.pathname;
+
+  if (excludedPages.includes(path)) {
+    return false;
+  }
+
+  if (!user) {
+    window.location.href = "/login.html";
+    return true;
+  } else {
+    return false;
+  }
+}
+
 export function setHomeUi(username: string | null): void {
   const welcome = document.getElementById("welcome-text") as HTMLHeadingElement;
   const loginAdvice = document.getElementById(
