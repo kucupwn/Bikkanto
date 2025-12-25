13a0a7ad088c827564d588df9bc692dca9e04729
Bence Gombkötő <kucukasd@gmail.com>
2025-12-25 09:42:31 +0100

    fix wrong urls

--- Frontend/src/api/urls.ts
+++ Frontend/src/api/urls.ts
@@ -1,9 +1,13 @@
 // URLs goes here
 export const API_BASE_URL = "http://127.0.0.1:8000";
+
 export const EXERCISES_API_URL = `${API_BASE_URL}/exercises`;
-export const EXERCISES_CATEGORY_API_URL = `${API_BASE_URL}/exercises/categories`;
+export const EXERCISES_CATEGORY_API_URL = `${EXERCISES_API_URL}/categories`;
+
 export const HISTORY_API_URL = `${API_BASE_URL}/history`;
-export const USERS_API_URL = `${API_BASE_URL}/users`;
+
 export const TOKEN_API_URL = `${API_BASE_URL}/auth/token`;
-export const REGISTER_API_URL = `${API_BASE_URL}/auth/register`;
-export const CHANGE_PASSWORD_API_URL = `${API_BASE_URL}/change_password`;
+
+export const USERS_API_URL = `${API_BASE_URL}/users`;
+export const CHANGE_PASSWORD_API_URL = `${USERS_API_URL}/change_password`;
+export const REGISTER_API_URL = `${USERS_API_URL}/register`;
