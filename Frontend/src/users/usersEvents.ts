8149a7eaec439b1e74e0981a7c0c87022984922a
Bence Gombkötő <kucukasd@gmail.com>
2025-12-26 09:34:12 +0100

    create usersEvents.ts move event listeners here

--- Frontend/src/users/usersEvents.ts
+++ Frontend/src/users/usersEvents.ts
@@ -45,39 +45,3 @@ export function attachUserEventListeners(callback: UserCallbacks): void {
   bindEdit("lastname-edit", "last_name", "last name", "lastname-paragraph");
   bindEdit("email-edit", "email", "email", "email-paragraph");
 }
-
-export function setModalHeaderTitle(editData: string) {
-  const title = document.getElementById("profile-modal-label");
-  if (!title) return;
-
-  title.textContent = `Change ${editData}`;
-}
-
-export function setCurrentProfileTextContent(
-  element: HTMLElement | null,
-  value: string | undefined
-): void {
-  if (element) {
-    element.textContent = value || "";
-  }
-}
-
-export function getEditInput(editKey: string, currentValue: string): string {
-  return `<input id="profile-input" name="${editKey}" type="text" class="form-control" placeholder="${currentValue}">`;
-}
-
-export function getNewUserFormData(formData: HTMLFormElement) {
-  const fd = new FormData(formData);
-  const data: Record<string, any> = {};
-
-  fd.forEach((value, key) => (data[key] = value));
-
-  if (data.password !== data.password2) {
-    alert("Passwords do not match!");
-    return;
-  }
-
-  delete data.password2;
-
-  return data;
-}
