db8c2b35b1c888c89af74046064eb92d6fcf31bd
Bence Gombkötő <kucukasd@gmail.com>
2026-01-04 09:44:11 +0100

    add title to stats modal

--- Frontend/src/history/historyStats.ts
+++ Frontend/src/history/historyStats.ts
@@ -4,6 +4,11 @@ export function openStatsModal(): void {
   const modalEl = document.getElementById("stats-modal");
   if (!modalEl) return;
 
+  const title = document.getElementById("stats-modal-label");
+  if (!title) return;
+
+  title.textContent = "Select history interval";
+
   const bootstrapModal = new Modal(modalEl);
   bootstrapModal.show();
 }
