e1bf8a5a523a0725389902ec96eada05d2fa8138
Bence Gombkötő <kucukasd@gmail.com>
2026-01-09 14:17:34 +0100

    add fetchHistoryRange api call in history api

--- Frontend/src/history/historyApi.ts
+++ Frontend/src/history/historyApi.ts
@@ -5,8 +5,9 @@ import type { WorkoutHistory } from "../types/history.types";
 export async function fetchAllHistory(
   apiUrl: string
 ): Promise<WorkoutHistory[]> {
+  const token = localStorage.getItem("token");
+
   try {
-    const token = localStorage.getItem("token");
     const data = await apiRequest<WorkoutHistory[]>(apiUrl, {
       method: "GET",
       headers: {
@@ -42,6 +43,36 @@ export async function postBatchHistory(
     showFeedback("History record(s) added successfully", "success");
   } catch (err: any) {
     showFeedback("Failed to add history records", "error");
+    console.error(err.message);
+  }
+}
+
+export async function fetchHistoryRange(
+  apiUrl: string,
+  startDate: string,
+  endDate: string
+) {
+  const token = localStorage.getItem("token");
+  const params = new URLSearchParams({
+    start_date: startDate,
+    end_date: endDate,
+  });
+
+  try {
+    const data = await apiRequest<WorkoutHistory[]>(
+      `${apiUrl}?${params.toString()}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    return data;
+  } catch (err: any) {
+    showFeedback("History range could not be fetched", "error");
     console.error(err.message);
   }
 }
