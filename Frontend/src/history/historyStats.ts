5eccd8138b1ab1de710b5cb80027c386a1cfff87
Bence Gombkötő <kucukasd@gmail.com>
2026-01-09 14:17:56 +0100

    handle date range submitting

--- Frontend/src/history/historyStats.ts
+++ Frontend/src/history/historyStats.ts
@@ -2,6 +2,10 @@ import { Modal } from "bootstrap";
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
+import { fetchHistoryRange } from "./historyApi";
+import { HISTORY_RANGE_URL } from "../api/urls";
+import { showFeedback } from "../ribbon/feedbackRibbon";
+
 export function openStatsModal(): void {
   const modalEl = document.getElementById("stats-modal");
   if (!modalEl) return;
@@ -15,17 +19,40 @@ export function openStatsModal(): void {
   bootstrapModal.show();
 
   getDatePicker();
+  handleRangeSubmitForm(bootstrapModal);
 }
 
 function getDatePicker(): void {
-  const dateInput = document.getElementById("history-date-picker");
+  const dateInput = document.getElementById(
+    "history-date-picker"
+  ) as HTMLInputElement | null;
   if (!dateInput) return;
 
   flatpickr(dateInput, {
     dateFormat: "Y-m-d",
     mode: "range",
-    onChange: (_, dateStr) => {
-      console.log(dateStr);
-    },
   });
+}
+
+function handleRangeSubmitForm(modal: bootstrap.Modal): void {
+  const form = document.getElementById("stats-form") as HTMLFormElement | null;
+  if (!form) return;
+
+  form.onsubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const range = formData.get("date-range");
+
+    if (!range || typeof range !== "string" || !range.includes(" to ")) {
+      showFeedback("Please select a valid date range", "error");
+      return;
+    }
+
+    const [start_date, end_date] = range.split(" to ");
+
+    await fetchHistoryRange(HISTORY_RANGE_URL, start_date, end_date);
+
+    modal.hide();
+    form.reset();
+  };
 }
