c3273229f25d145d55adbea16238a855578dd272
Bence Gombkötő <kucukasd@gmail.com>
2026-01-10 13:47:53 +0100

    use difficulty class for history schema type

--- Backend/schemas/history_schema.py
+++ Backend/schemas/history_schema.py
@@ -1,13 +1,14 @@
 from pydantic import BaseModel, Field
 from datetime import date
-from .exercises_schema import Category
+from .exercises_schema import Category, Difficulty
 
 
 class HistoryBase(BaseModel):
     date_complete: date
-    cycles: int
-    category: Category
     exercise: str
+    category: Category
+    difficulty: Difficulty
+    cycles: int
     repetitions: int
     sum_repetitions: int
 
@@ -21,8 +22,9 @@ class HistoryRead(HistoryBase):
 
 class HistoryCreate(HistoryBase):
     date_complete: date = Field(...)
-    cycles: int = Field(..., gt=0)
-    category: Category
     exercise: str = Field(..., min_length=1)
+    category: Category
+    difficulty: Difficulty
+    cycles: int = Field(..., gt=0)
     repetitions: int = Field(..., gt=0)
     sum_repetitions: int = Field(..., gt=0)
