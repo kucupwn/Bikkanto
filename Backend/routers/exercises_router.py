0b33511631161e88da521b3936d107d3a87f28fd
Bence Gombkötő <kucukasd@gmail.com>
2026-01-13 17:37:54 +0100

    fix exc endpoints for category and overall structure, add categories endpoints

--- Backend/routers/exercises_router.py
+++ Backend/routers/exercises_router.py
@@ -1,30 +1,46 @@
-from typing import Annotated
-from sqlalchemy.orm import Session
+from typing import Annotated, List
+from sqlalchemy.orm import Session, joinedload
 from fastapi import Depends, APIRouter, HTTPException, Path
 from starlette import status
 from ..database import get_db
-from ..models import Exercises
+from ..models import Exercises, Categories
 from ..schemas.exercises_schema import (
     ExerciseRead,
     ExerciseCreate,
     ExerciseUpdate,
-    Category,
+    CategoryRead,
+    CategoryCreate,
 )
 
 router = APIRouter(prefix="/exercises", tags=["exercises"])
-
-
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
-@router.get("/categories")
-def get_categories():
-    return [c.value for c in Category]
+@router.get(
+    "/categories", response_model=List[CategoryRead], status_code=status.HTTP_200_OK
+)
+async def read_all_categories(db: db_dependency):
+    return db.query(Categories).all()
 
 
-@router.get("/", response_model=list[ExerciseRead], status_code=status.HTTP_200_OK)
-async def read_all(db: db_dependency):
-    return db.query(Exercises).all()
+@router.post("/categories", response_model=CategoryRead, status_code=status.HTTP_200_OK)
+async def create_category(db: db_dependency, category_create: CategoryCreate):
+    existing = db.query(Categories).filter_by(name=category_create.name).first()
+    if existing:
+        raise HTTPException(status_code=400, detail="Category already exists")
+
+    category_model = Categories(**category_create.model_dump())
+    db.add(category_model)
+    db.commit()
+    db.refresh(category_model)
+
+    return category_model
+
+
+@router.get("/", response_model=List[ExerciseRead], status_code=status.HTTP_200_OK)
+async def read_all_exercises(db: db_dependency):
+    exercises = db.query(Exercises).options(joinedload(Exercises.category)).all()
+    return exercises
 
 
 @router.post("/", response_model=ExerciseRead, status_code=status.HTTP_201_CREATED)
@@ -36,6 +52,10 @@ async def create_exercise(db: db_dependency, exercise_create: ExerciseCreate):
     )
     if existing:
         raise HTTPException(status_code=400, detail="Exercise already exists")
+
+    category = db.get(Categories, exercise_create.category_id)
+    if not category:
+        raise HTTPException(status_code=400, detail="Invalid category id")
 
     exercise_model = Exercises(**exercise_create.model_dump())
     db.add(exercise_model)
@@ -55,6 +75,10 @@ async def update_exercise(
 
     if exercise_model is None:
         raise HTTPException(status_code=404, detail="Exercise not found")
+
+    category = db.get(Categories, exercise_update.category_id)
+    if not category:
+        raise HTTPException(status_code=400, detail="Invalid category id")
 
     update_data = exercise_update.model_dump(exclude_unset=True)
 
@@ -77,6 +101,6 @@ async def delete_exercise(db: db_dependency, exercise_id: int = Path(gt=0)):
     if not exercise_model:
         raise HTTPException(status_code=404, detail="Exercise not found")
 
-    db.query(Exercises).filter(Exercises.id == exercise_id).delete()
+    db.delete(exercise_model)
 
     db.commit()
