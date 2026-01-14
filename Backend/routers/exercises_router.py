6bb33c1e7d423cd0a18d4783eb542394b5bf068e
Bence Gombkötő <kucukasd@gmail.com>
2026-01-14 13:09:32 +0100

    add user dependency to exc router endpoints

--- Backend/routers/exercises_router.py
+++ Backend/routers/exercises_router.py
@@ -2,6 +2,7 @@ from typing import Annotated, List
 from sqlalchemy.orm import Session, joinedload
 from fastapi import Depends, APIRouter, HTTPException, Path
 from starlette import status
+from .auth import get_current_user
 from ..database import get_db
 from ..models import Exercises, Categories
 from ..schemas.exercises_schema import (
@@ -14,22 +15,42 @@ from ..schemas.exercises_schema import (
 
 router = APIRouter(prefix="/exercises", tags=["exercises"])
 db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
 
 
 @router.get(
     "/categories", response_model=List[CategoryRead], status_code=status.HTTP_200_OK
 )
-async def read_all_categories(db: db_dependency):
-    return db.query(Categories).all()
+async def read_all_categories(user: user_dependency, db: db_dependency):
+    if user is None:
+        raise HTTPException(status_code=401, detail="Authentication Failed")
+
+    return db.query(Categories).filter(Categories.user_id == user.get("id")).all()
 
 
 @router.post("/categories", response_model=CategoryRead, status_code=status.HTTP_200_OK)
-async def create_category(db: db_dependency, category_create: CategoryCreate):
-    existing = db.query(Categories).filter_by(name=category_create.name).first()
+async def create_category(
+    user: user_dependency, db: db_dependency, category_create: CategoryCreate
+):
+    if user is None:
+        raise HTTPException(status_code=401, detail="Authentication Failed")
+
+    existing = (
+        db.query(Categories)
+        .filter(
+            Categories.name == category_create.name,
+            Categories.user_id == user.get("id"),
+        )
+        .first()
+    )
+
     if existing:
         raise HTTPException(status_code=400, detail="Category already exists")
 
-    category_model = Categories(**category_create.model_dump())
+    category_model = Categories(
+        **category_create.model_dump(),
+        user_id=user.get("id"),
+    )
     db.add(category_model)
     db.commit()
     db.refresh(category_model)
@@ -38,26 +59,51 @@ async def create_category(db: db_dependency, category_create: CategoryCreate):
 
 
 @router.get("/", response_model=List[ExerciseRead], status_code=status.HTTP_200_OK)
-async def read_all_exercises(db: db_dependency):
-    exercises = db.query(Exercises).options(joinedload(Exercises.category)).all()
+async def read_all_exercises(user: user_dependency, db: db_dependency):
+    if user is None:
+        raise HTTPException(status_code=401, detail="Authentication Failed")
+
+    exercises = (
+        db.query(Exercises)
+        .filter(Exercises.user_id == user.get("id"))
+        .options(joinedload(Exercises.category))
+        .all()
+    )
     return exercises
 
 
 @router.post("/", response_model=ExerciseRead, status_code=status.HTTP_201_CREATED)
-async def create_exercise(db: db_dependency, exercise_create: ExerciseCreate):
+async def create_exercise(
+    user: user_dependency, db: db_dependency, exercise_create: ExerciseCreate
+):
+    if user is None:
+        raise HTTPException(status_code=401, detail="Authentication Failed")
+
     existing = (
         db.query(Exercises)
-        .filter_by(exercise_name=exercise_create.exercise_name)
+        .filter(
+            Exercises.exercise_name == exercise_create.exercise_name,
+            Exercises.user_id == user.get("id"),
+        )
         .first()
     )
+
     if existing:
         raise HTTPException(status_code=400, detail="Exercise already exists")
 
-    category = db.get(Categories, exercise_create.category_id)
+    category = (
+        db.query(Categories)
+        .filter(
+            Categories.id == exercise_create.category_id,
+            Categories.user_id == user.get("id"),
+        )
+        .first()
+    )
+
     if not category:
         raise HTTPException(status_code=400, detail="Invalid category id")
 
-    exercise_model = Exercises(**exercise_create.model_dump())
+    exercise_model = Exercises(**exercise_create.model_dump(), user_id=user.get("id"))
     db.add(exercise_model)
     db.commit()
     db.refresh(exercise_model)
@@ -69,14 +115,35 @@ async def create_exercise(db: db_dependency, exercise_create: ExerciseCreate):
     "/{exercise_id}", response_model=ExerciseRead, status_code=status.HTTP_200_OK
 )
 async def update_exercise(
-    db: db_dependency, exercise_update: ExerciseUpdate, exercise_id: int = Path(gt=0)
+    user: user_dependency,
+    db: db_dependency,
+    exercise_update: ExerciseUpdate,
+    exercise_id: int = Path(gt=0),
 ):
-    exercise_model = db.query(Exercises).filter(Exercises.id == exercise_id).first()
+    if user is None:
+        raise HTTPException(status_code=401, detail="Authentication Failed")
+
+    exercise_model = (
+        db.query(Exercises)
+        .filter(
+            Exercises.id == exercise_id,
+            Exercises.user_id == user.get("id"),
+        )
+        .first()
+    )
 
     if exercise_model is None:
         raise HTTPException(status_code=404, detail="Exercise not found")
 
-    category = db.get(Categories, exercise_update.category_id)
+    category = (
+        db.query(Categories)
+        .filter(
+            Categories.id == exercise_update.category_id,
+            Categories.user_id == user.get("id"),
+        )
+        .first()
+    )
+
     if not category:
         raise HTTPException(status_code=400, detail="Invalid category id")
 
@@ -95,8 +162,20 @@ async def update_exercise(
     "/{exercise_id}",
     status_code=status.HTTP_204_NO_CONTENT,
 )
-async def delete_exercise(db: db_dependency, exercise_id: int = Path(gt=0)):
-    exercise_model = db.query(Exercises).filter(Exercises.id == exercise_id).first()
+async def delete_exercise(
+    user: user_dependency, db: db_dependency, exercise_id: int = Path(gt=0)
+):
+    if user is None:
+        raise HTTPException(status_code=401, detail="Authentication Failed")
+
+    exercise_model = (
+        db.query(Exercises)
+        .filter(
+            Exercises.id == exercise_id,
+            Exercises.user_id == user.get("id"),
+        )
+        .first()
+    )
 
     if not exercise_model:
         raise HTTPException(status_code=404, detail="Exercise not found")
