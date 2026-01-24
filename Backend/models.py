2310cab1c5e4738fafa1edcdbe4e200e0bc0ff0b
Bence Gombkötő <kucukasd@gmail.com>
2026-01-24 22:01:50 +0100

    remove default user role from model, it is set explicitly in endpoint

--- Backend/models.py
+++ Backend/models.py
@@ -97,7 +97,7 @@ class Users(Base):
     email = Column(String, unique=True)
     first_name = Column(String)
     last_name = Column(String)
-    role = Column(String, default="user")
+    role = Column(String)
     hashed_password = Column(String)
 
     history = relationship(
