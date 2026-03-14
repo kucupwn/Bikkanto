f1ac0a777a3cb0f6ea2f504d8f2c8cbc884785e6
Bence Gombkötő <kucukasd@gmail.com>
2026-03-14 14:38:48 +0100

    use routes for pages

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,5 +1,10 @@
 import styled from "styled-components";
-import { Navbar } from "./Navbar";
+import { Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { Home } from "./pages/Home";
+import { Roll } from "./pages/Roll";
+import { Exercises } from "./pages/Exercises";
+import { History } from "./pages/History";
 
 const Title = styled.h1`
   font-size: 48px;
@@ -11,7 +16,14 @@ export function App() {
   return (
     <>
       <Title>Bikkanto</Title>
-      <Navbar></Navbar>;
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/roll" element={<Roll />} />
+        <Route path="/exercises" element={<Exercises />} />
+        <Route path="/history" element={<History />} />
+      </Routes>
     </>
   );
 }
