deac276e62a759984dcd845e7582c34bc6888e15
Bence Gombkötő <kucukasd@gmail.com>
2026-03-15 09:51:45 +0100

    add col headers to table

--- Frontend/src/pages/Exercises.tsx
+++ Frontend/src/pages/Exercises.tsx
@@ -4,12 +4,23 @@ import { api } from "../api/api";
 
 export function Exercises() {
   const [data, setData] = useState<any[]>([]);
+  const [columns, setColumns] = useState<any[]>([]);
 
   useEffect(() => {
     async function fetchExercises() {
       try {
         const res = await api.get("/exercises");
-        setData(res.data);
+        const exercises = res.data;
+
+        setData(exercises);
+
+        if (exercises.length > 0) {
+          const cols = Object.keys(exercises[0]).map((key) => ({
+            data: key,
+            title: key.charAt(0).toUpperCase() + key.slice(1),
+          }));
+          setColumns(cols);
+        }
       } catch (err) {
         console.error(err);
       }
@@ -21,7 +32,7 @@ export function Exercises() {
   return (
     <>
       <h1>Exercises</h1>
-      <DataTable data={data} />
+      <DataTable data={data} colHeaders={columns} columns={columns} />
     </>
   );
 }
