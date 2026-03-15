55eab6864e9676dcaf5845832e88a1b5c01bf372
Bence Gombkötő <kucukasd@gmail.com>
2026-03-15 09:52:07 +0100

    setup hottable properly

--- Frontend/src/components/DataTable.tsx
+++ Frontend/src/components/DataTable.tsx
@@ -1,18 +1,29 @@
-import { HotTable } from "@handsontable/react";
+import { registerAllModules } from "handsontable/registry";
+registerAllModules();
+
+import { HotTable } from "@handsontable/react-wrapper";
 import "handsontable/styles/handsontable.min.css";
 import "handsontable/styles/ht-theme-main.min.css";
 
-interface Prop {
+interface Props {
   data: any;
+  colHeaders: string[];
+  columns: any[];
 }
 
-export function DataTable({ data }: Prop) {
+export function DataTable({ data, colHeaders, columns }: Props) {
   return (
-    <HotTable
-      data={data}
-      colHeaders={true}
-      rowHeaders={true}
-      licenseKey="non-commercial-and-evaluation"
-    />
+    <div style={{ width: "80%", margin: "auto" }}>
+      <HotTable
+        data={data}
+        colHeaders={colHeaders}
+        columns={columns}
+        rowHeaders={true}
+        width="100%"
+        height="auto"
+        stretchH="all"
+        licenseKey="non-commercial-and-evaluation"
+      />
+    </div>
   );
 }
