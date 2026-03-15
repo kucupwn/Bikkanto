c6a1832fac5764c0f4de606027fc73436ffbf17b
Bence Gombkötő <kucukasd@gmail.com>
2026-03-15 09:14:31 +0100

    first exc fetch

--- Frontend/src/pages/Exercises.tsx
+++ Frontend/src/pages/Exercises.tsx
@@ -1,3 +1,27 @@
+import { useEffect, useState } from "react";
+import { DataTable } from "../components/DataTable";
+import { api } from "../api/api";
+
 export function Exercises() {
-  return <h1>Exercises</h1>;
+  const [data, setData] = useState<any[]>([]);
+
+  useEffect(() => {
+    async function fetchExercises() {
+      try {
+        const res = await api.get("/exercises");
+        setData(res.data);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    fetchExercises();
+  }, []);
+
+  return (
+    <>
+      <h1>Exercises</h1>
+      <DataTable data={data} />
+    </>
+  );
 }
