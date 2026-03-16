892469663590629a184c72647a5c12f961839f8f
Bence Gombkötő <kucukasd@gmail.com>
2026-03-16 14:42:54 +0100

    create basic inputs for roll page

--- Frontend/src/pages/Roll.tsx
+++ Frontend/src/pages/Roll.tsx
@@ -1,3 +1,40 @@
+import styled from "styled-components";
+
+const WorkoutBasePropertiesWrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  justify-self: center;
+  margin: 1rem;
+  width: 60vw;
+`;
+
+const ExerciseCountWrapper = styled.div`
+  display: flex;
+  gap: 1rem;
+`;
+
+const ExerciseCountDifficultyWrapper = styled.div`
+  display: flex;
+  gap: 1rem;
+`;
+
 export function Roll() {
-  return <h1>Roll</h1>;
+  return (
+    <>
+      <WorkoutBasePropertiesWrapper>
+        <ExerciseCountWrapper>
+          <span>Exercise Count:</span>
+          <input type="number" />
+        </ExerciseCountWrapper>
+        <ExerciseCountDifficultyWrapper>
+          <span>Exercise Count Difficulty:</span>
+          <select name="exercise-count-difficulty">
+            <option value="easy">Easy</option>
+            <option value="medium">Medium</option>
+            <option value="hard">Hard</option>
+          </select>
+        </ExerciseCountDifficultyWrapper>
+      </WorkoutBasePropertiesWrapper>
+    </>
+  );
 }
