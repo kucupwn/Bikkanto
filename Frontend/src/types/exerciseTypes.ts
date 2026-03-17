32a38d65f2be2917338e9f26818204596a72ab09
Bence Gombkötő <kucukasd@gmail.com>
2026-03-17 16:10:21 +0100

    move current types into separate file for types

--- Frontend/src/types/exerciseTypes.ts
+++ Frontend/src/types/exerciseTypes.ts
@@ -1,3 +1,33 @@
+export interface Exercises {
+  id: number;
+  exercise_name: string;
+  difficulty: Difficulty;
+  easy_min: number;
+  easy_max: number;
+  medium_min: number;
+  medium_max: number;
+  hard_min: number;
+  hard_max: number;
+  category_id: number;
+  category_name: string;
+}
+
+export interface Category {
+  id: number;
+  category_name: string;
+}
+
+export const exerciseCountDifficultyOptions = [
+  "easy",
+  "medium",
+  "hard",
+] as const;
+export type ExerciseCountDifficulty =
+  (typeof exerciseCountDifficultyOptions)[number];
+
+export const difficultyOptions = ["beginner", "advanced", "pro"] as const;
+export type Difficulty = (typeof difficultyOptions)[number];
+
 export const NUMERIC_COLUMNS = [
   "easy_min",
   "easy_max",
