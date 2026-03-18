73994af714b3163d1efbce5d9a37846cfccd7b73
Bence Gombkötő <kucukasd@gmail.com>
2026-03-18 16:03:29 +0100

    move existing roll logic into separate comp: WorkoutSettings

---
+++ Frontend/src/components/roll/WorkoutSettings.tsx
@@ -0,0 +1,89 @@
+import { type ChangeEvent, type Dispatch, type SetStateAction } from "react";
+import styled from "styled-components";
+import {
+  exerciseRepetitionDifficultyOptions,
+  type Category,
+} from "../../types/exerciseTypes";
+import { capitalize } from "../../utils";
+import { CategorySelection } from "./CategorySelection";
+
+interface Props {
+  exerciseCount: number | "";
+  setExerciseCount: Dispatch<SetStateAction<number | "">>;
+  categories: Category[];
+}
+
+const WorkoutBasePropertiesWrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin: 1rem;
+  width: 60vw;
+`;
+
+const ExerciseCountWrapper = styled.div`
+  display: flex;
+  gap: 1rem;
+`;
+
+const ExerciseCountDifficultyWrapper = styled.div`
+  display: flex;
+  gap: 1rem;
+`;
+
+const GetButton = styled.button`
+  font-size: 20px;
+  padding: 0.3rem 0.5rem;
+  margin: 1rem;
+  cursor: pointer;
+`;
+
+export function WorkoutSettings({
+  exerciseCount,
+  setExerciseCount,
+  categories,
+}: Props) {
+  const safeCount = typeof exerciseCount == "number" ? exerciseCount : 0;
+
+  function handleExerciseCountChange(e: ChangeEvent<HTMLInputElement>) {
+    const currentValue = Number(e.target.value);
+
+    if (currentValue <= 0) {
+      setExerciseCount("");
+    } else {
+      setExerciseCount(currentValue);
+    }
+  }
+
+  return (
+    <>
+      <WorkoutBasePropertiesWrapper>
+        <ExerciseCountWrapper>
+          <span>Exercise Count:</span>
+          <input
+            type="number"
+            value={exerciseCount}
+            onChange={handleExerciseCountChange}
+          />
+        </ExerciseCountWrapper>
+        <ExerciseCountDifficultyWrapper>
+          <span>Exercise Repetitions Difficulty:</span>
+          <select name="exercise-count-difficulty">
+            {exerciseRepetitionDifficultyOptions.map((diff) => (
+              <option key={diff} value={diff}>
+                {capitalize(diff)}
+              </option>
+            ))}
+          </select>
+        </ExerciseCountDifficultyWrapper>
+      </WorkoutBasePropertiesWrapper>
+      {Array.from({ length: safeCount }).map((_, idx) => (
+        <CategorySelection
+          key={idx}
+          currentCount={idx + 1}
+          categories={categories}
+        />
+      ))}
+      {safeCount > 0 && <GetButton>Get</GetButton>}
+    </>
+  );
+}
--- Frontend/src/pages/Roll.tsx
+++ Frontend/src/pages/Roll.tsx
@@ -1,14 +1,12 @@
 import styled from "styled-components";
-import { useEffect, useState, type ChangeEvent } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../api/api";
-import { CategorySelection } from "../components/roll/CategorySelection";
 import {
-  exerciseRepetitionDifficultyOptions,
   type Category,
   type ExerciseRepetitionDifficulty,
   type Exercises,
 } from "../types/exerciseTypes";
-import { capitalize } from "../utils";
+import { WorkoutSettings } from "../components/roll/WorkoutSettings";
 
 const RollContainer = styled.div`
   display: flex;
@@ -17,48 +15,12 @@ const RollContainer = styled.div`
   flex-direction: column;
 `;
 
-const WorkoutBasePropertiesWrapper = styled.div`
-  display: flex;
-  justify-content: space-around;
-  margin: 1rem;
-  width: 60vw;
-`;
-
-const ExerciseCountWrapper = styled.div`
-  display: flex;
-  gap: 1rem;
-`;
-
-const ExerciseCountDifficultyWrapper = styled.div`
-  display: flex;
-  gap: 1rem;
-`;
-
-const GetButton = styled.button`
-  font-size: 20px;
-  padding: 0.3rem 0.5rem;
-  margin: 1rem;
-  cursor: pointer;
-`;
-
 export function Roll() {
   const [exerciseCount, setExerciseCount] = useState<number | "">("");
   const [exerciseCountDifficulty, setExerciseCountDifficulty] =
     useState<ExerciseRepetitionDifficulty>("easy");
   const [categories, setCategories] = useState<Category[]>([]);
   const [exercises, setExercises] = useState<Exercises[]>([]);
-
-  const safeCount = typeof exerciseCount == "number" ? exerciseCount : 0;
-
-  function handleExerciseCountChange(e: ChangeEvent<HTMLInputElement>) {
-    const currentValue = Number(e.target.value);
-
-    if (currentValue <= 0) {
-      setExerciseCount("");
-    } else {
-      setExerciseCount(currentValue);
-    }
-  }
 
   useEffect(() => {
     async function fetchCategories() {
@@ -93,34 +55,11 @@ export function Roll() {
   return (
     <>
       <RollContainer>
-        <WorkoutBasePropertiesWrapper>
-          <ExerciseCountWrapper>
-            <span>Exercise Count:</span>
-            <input
-              type="number"
-              value={exerciseCount}
-              onChange={handleExerciseCountChange}
-            />
-          </ExerciseCountWrapper>
-          <ExerciseCountDifficultyWrapper>
-            <span>Exercise Repetitions Difficulty:</span>
-            <select name="exercise-count-difficulty">
-              {exerciseRepetitionDifficultyOptions.map((diff) => (
-                <option key={diff} value={diff}>
-                  {capitalize(diff)}
-                </option>
-              ))}
-            </select>
-          </ExerciseCountDifficultyWrapper>
-        </WorkoutBasePropertiesWrapper>
-        {Array.from({ length: safeCount }).map((_, idx) => (
-          <CategorySelection
-            key={idx}
-            currentCount={idx + 1}
-            categories={categories}
-          />
-        ))}
-        {safeCount > 0 && <GetButton>Get</GetButton>}
+        <WorkoutSettings
+          exerciseCount={exerciseCount}
+          setExerciseCount={setExerciseCount}
+          categories={categories}
+        />
       </RollContainer>
     </>
   );
