89fffc03868503c6b5d6c62f6e7a6c60cafa34c1
Bence Gombkötő <kucukasd@gmail.com>
2026-03-23 14:39:59 +0100

    style summary table

--- Frontend/src/components/roll/SummaryTable.tsx
+++ Frontend/src/components/roll/SummaryTable.tsx
@@ -7,27 +7,42 @@ interface Props {
 
 const WorkoutTable = styled.table`
   margin: 1rem;
+  border-collapse: collapse;
+`;
+
+const TableHeader = styled.th`
+  border: 1px solid black;
+  padding: 8px;
+  text-align: center;
+  vertical-align: center;
+`;
+
+const Cell = styled.td`
+  border: 1px solid black;
+  padding: 8px;
+  text-align: center;
+  vertical-align: center;
 `;
 
 export function SummaryTable({ workout }: Props) {
   return (
-    <WorkoutTable border={2}>
+    <WorkoutTable>
       <thead>
         <tr>
-          <th>Exercise Name</th>
-          <th>Exercise Difficulty</th>
-          <th>Reps Difficulty</th>
-          <th>Reps</th>
+          <TableHeader>Exercise Name</TableHeader>
+          <TableHeader>Exercise Difficulty</TableHeader>
+          <TableHeader>Reps Difficulty</TableHeader>
+          <TableHeader>Reps</TableHeader>
         </tr>
       </thead>
 
       <tbody>
         {workout.map((entry, index) => (
           <tr key={index}>
-            <td>{entry.exercise_name}</td>
-            <td>{entry.difficulty}</td>
-            <td>{entry.reps_difficulty}</td>
-            <td>{entry.reps}</td>
+            <Cell>{entry.exercise_name}</Cell>
+            <Cell>{entry.difficulty}</Cell>
+            <Cell>{entry.reps_difficulty}</Cell>
+            <Cell>{entry.reps}</Cell>
           </tr>
         ))}
       </tbody>
