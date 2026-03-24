3e1783747bf274cddd3de925ce6e15b294961cc2
Bence Gombkötő <kucukasd@gmail.com>
2026-03-24 15:23:52 +0100

    style layout of stored workout elements

--- Frontend/src/components/roll/StoredWorkout.tsx
+++ Frontend/src/components/roll/StoredWorkout.tsx
@@ -1,6 +1,17 @@
 import type { Dispatch, SetStateAction } from "react";
 import type { ViewModes } from "../../pages/Roll";
 import type { WorkoutEntry } from "../../types/exerciseTypes";
+import styled from "styled-components";
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  gap: 1rem;
+  margin: 1rem;
+`;
+
+const TextWrapper = styled.div`
+  margin: 1rem;
+`;
 
 interface Props {
   setWorkout: Dispatch<SetStateAction<WorkoutEntry[] | null>>;
@@ -30,10 +41,14 @@ export function StoredWorkout({
 
   return (
     <>
-      <p>You have accepted, but not saved workout.</p>
-      <p>Do you want to Discard it, or Recall it?</p>
-      <button onClick={discardWorkout}>Discard</button>
-      <button onClick={recallWorkout}>Recall</button>
+      <TextWrapper>
+        <p>You have accepted, but not saved workout.</p>
+        <p>Do you want to Discard it, or Recall it?</p>
+      </TextWrapper>
+      <ButtonWrapper>
+        <button onClick={discardWorkout}>Discard</button>
+        <button onClick={recallWorkout}>Recall</button>
+      </ButtonWrapper>
     </>
   );
 }
