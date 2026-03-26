ac12268144180de0f94767169958f0a57ffd1a83
Bence Gombkötő <kucukasd@gmail.com>
2026-03-26 15:14:49 +0100

    fetch categories on exercises page

--- Frontend/src/pages/Exercises.tsx
+++ Frontend/src/pages/Exercises.tsx
@@ -1,7 +1,11 @@
 import { useEffect, useState } from "react";
 import { DataTable } from "../components/DataTable";
 import { api } from "../api/api";
-import { EXERCISE_COLUMNS_ORDER, type Exercise } from "../types/exerciseTypes";
+import {
+  EXERCISE_COLUMNS_ORDER,
+  type Category,
+  type Exercise,
+} from "../types/exerciseTypes";
 import { capitalize } from "../utils";
 import styled from "styled-components";
 import { CategoryModal } from "../components/exercise/CategoryModal";
@@ -21,9 +25,10 @@ const Button = styled.button`
 type ModalType = "category" | "add" | "edit" | "delete" | null;
 
 export function Exercises() {
-  const [data, setData] = useState<Exercise[]>([]);
   const [columns, setColumns] = useState<any[]>([]);
   const [activeModal, setActiveModal] = useState<ModalType>(null);
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [exercises, setExercises] = useState<Exercise[]>([]);
 
   useEffect(() => {
     async function fetchExercises() {
@@ -31,7 +36,7 @@ export function Exercises() {
         const res = await api.get("/exercises");
         const exercises = res.data;
 
-        setData(exercises);
+        setExercises(exercises);
 
         if (exercises.length > 0) {
           const cols = EXERCISE_COLUMNS_ORDER.map((key) => ({
@@ -48,6 +53,21 @@ export function Exercises() {
     fetchExercises();
   }, []);
 
+  useEffect(() => {
+    async function fetchCategories() {
+      try {
+        const res = await api("/exercises/categories");
+        const categories = res.data;
+
+        setCategories(categories);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    fetchCategories();
+  }, []);
+
   return (
     <>
       <ButtonWrapper>
@@ -56,10 +76,11 @@ export function Exercises() {
         <Button onClick={() => setActiveModal("edit")}>Edit</Button>
         <Button onClick={() => setActiveModal("delete")}>Delete</Button>
       </ButtonWrapper>
-      <DataTable data={data} columns={columns} />
+      <DataTable data={exercises} columns={columns} />
       <CategoryModal
         isOpen={activeModal === "category"}
         onClose={() => setActiveModal(null)}
+        categories={categories}
       />
     </>
   );
