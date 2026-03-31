30290d32fb39c289e118b81a2ceb9bd14712002e
Bence Gombkötő <kucukasd@gmail.com>
2026-03-31 14:34:01 +0200

    add edit exc modal

---
+++ Frontend/src/components/exercise/EditExerciseModal.tsx
@@ -0,0 +1,214 @@
+import styled from "styled-components";
+import {
+  type Exercise,
+  type Category,
+  exerciseDifficultyOptions,
+} from "../../types/exerciseTypes";
+import { useState, type ChangeEvent } from "react";
+import { capitalize } from "../../utils";
+
+interface Props {
+  isOpen: boolean;
+  onClose: () => void;
+  exercises: Exercise[];
+  categories: Category[];
+}
+
+const Overlay = styled.div`
+  position: fixed;
+  inset: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 1000;
+`;
+
+const ModalBox = styled.div`
+  background-color: white;
+  padding: 1rem;
+  border-radius: 12px;
+  min-width: 300px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h2`
+  margin-bottom: 1rem;
+`;
+
+const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const ExercisePropsContainer = styled.div`
+  display: grid;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(4, 1fr);
+  gap: 1rem;
+  margin-top: 1rem;
+`;
+
+const Button = styled.button`
+  margin-top: 1rem;
+`;
+
+export function EditExerciseModal({
+  isOpen,
+  onClose,
+  exercises,
+  categories,
+}: Props) {
+  if (!isOpen) return null;
+
+  const [editedExercise, setEditedExercise] = useState<Exercise>(exercises[0]);
+
+  function handleChange(e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) {
+    const { name, value } = e.target;
+
+    if (name === "exercise_id") {
+      const selected = exercises.find((ex) => ex.id === Number(value));
+      if (selected) {
+        setEditedExercise(selected);
+      }
+      return;
+    }
+
+    setEditedExercise((prev) => {
+      if (name === "category_id") {
+        const selected = categories.find((cat) => cat.id === Number(value));
+        return {
+          ...prev,
+          category_id: Number(value),
+          category_name: selected?.category_name ?? "",
+        };
+      }
+
+      return {
+        ...prev,
+        [name]: e.target.type === "number" ? Number(value) : value,
+      };
+    });
+  }
+
+  function handleEditExercise() {
+    const existing = exercises.find(
+      (ex) => ex.exercise_name === editedExercise.exercise_name.toLowerCase(),
+    );
+
+    if (existing) {
+      console.error(`${editedExercise.exercise_name} already exists.`);
+    } else {
+      console.log(editedExercise);
+      onClose();
+    }
+  }
+
+  return (
+    <Overlay onClick={onClose}>
+      <ModalBox onClick={(e) => e.stopPropagation()}>
+        <Title>Edit exercise</Title>
+        <select
+          name="exercise_id"
+          value={editedExercise.exercise_name}
+          onChange={handleChange}
+        >
+          {exercises.map((ex) => (
+            <option key={ex.id} value={ex.id}>
+              {capitalize(ex.exercise_name)}
+            </option>
+          ))}
+        </select>
+        <ExercisePropsContainer>
+          <InputWrapper>
+            <span>Category</span>
+            <select
+              name="category_id"
+              value={editedExercise.category_id}
+              onChange={handleChange}
+            >
+              {categories.map((cat) => (
+                <option key={cat.id} value={cat.id}>
+                  {capitalize(cat.category_name)}
+                </option>
+              ))}
+            </select>
+          </InputWrapper>
+          <InputWrapper>
+            <span>Difficulty</span>
+            <select
+              name="difficulty"
+              value={editedExercise.difficulty}
+              onChange={handleChange}
+            >
+              {exerciseDifficultyOptions.map((diff) => (
+                <option key={diff} value={diff}>
+                  {capitalize(diff)}
+                </option>
+              ))}
+            </select>
+          </InputWrapper>
+          <InputWrapper>
+            <span>Easy min reps</span>
+            <input
+              name="easy_min"
+              type="number"
+              value={editedExercise.easy_min}
+              onChange={handleChange}
+            />
+          </InputWrapper>
+          <InputWrapper>
+            <span>Easy max reps</span>
+            <input
+              name="easy_max"
+              type="number"
+              value={editedExercise.easy_max}
+              onChange={handleChange}
+            />
+          </InputWrapper>
+          <InputWrapper>
+            <span>Medium min reps</span>
+            <input
+              name="medium_min"
+              type="number"
+              value={editedExercise.medium_min}
+              onChange={handleChange}
+            />
+          </InputWrapper>
+          <InputWrapper>
+            <span>Medium max reps</span>
+            <input
+              name="medium_max"
+              type="number"
+              value={editedExercise.medium_max}
+              onChange={handleChange}
+            />
+          </InputWrapper>
+          <InputWrapper>
+            <span>Hard min reps</span>
+            <input
+              name="hard_min"
+              type="number"
+              value={editedExercise.hard_min}
+              onChange={handleChange}
+            />
+          </InputWrapper>
+          <InputWrapper>
+            <span>Hard max reps</span>
+            <input
+              name="hard_max"
+              type="number"
+              value={editedExercise.hard_max}
+              onChange={handleChange}
+            />
+          </InputWrapper>
+        </ExercisePropsContainer>
+        <Button onClick={handleEditExercise}>Add</Button>
+      </ModalBox>
+    </Overlay>
+  );
+}
