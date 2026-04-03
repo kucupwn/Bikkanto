d637802c71e5800fed181c6cd764e9d0476f47cb
Bence Gombkötő <kucukasd@gmail.com>
2026-04-03 11:08:15 +0200

    add first history table

--- Frontend/src/pages/History.tsx
+++ Frontend/src/pages/History.tsx
@@ -1,3 +1,35 @@
+import { useEffect, useState } from "react";
+import { api } from "../api/api";
+import { HISTORY_COLUMN_ORDER } from "../types/historyTypes";
+import { capitalize } from "../utils";
+import { DataTable } from "../components/DataTable";
+
 export function History() {
-  return <h1>History</h1>;
+  const [history, setHistory] = useState<History[]>([]);
+  const [columns, setColumns] = useState<any[]>([]);
+
+  async function fetchHistory() {
+    try {
+      const res = await api.get("/history");
+      const history = res.data;
+
+      setHistory(history);
+
+      if (history.length > 0) {
+        const cols = HISTORY_COLUMN_ORDER.map((key) => ({
+          data: key,
+          title: capitalize(key).replace("_", " "),
+        }));
+        setColumns(cols);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  useEffect(() => {
+    fetchHistory();
+  }, []);
+
+  return <DataTable data={history} columns={columns} />;
 }
