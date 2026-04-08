3c9912635d3bf0822be0fa3a9b0edbb042338719
Bence Gombkötő <kucukasd@gmail.com>
2026-04-08 20:02:54 +0200

    add user profile datas to placeholders

--- Frontend/src/pages/Profile.tsx
+++ Frontend/src/pages/Profile.tsx
@@ -1,4 +1,9 @@
 import styled from "styled-components";
+import type { User } from "../types/userTypes";
+
+interface Props {
+  currentUser: User;
+}
 
 const ProfileContainer = styled.div`
   display: flex;
@@ -11,22 +16,23 @@ const DataWrapper = styled.div`
   display: flex;
 `;
 
-export function Profile() {
+export function Profile({ currentUser }: Props) {
+  console.log(currentUser);
   return (
     <ProfileContainer>
       <span>Email</span>
       <DataWrapper>
-        <input type="text" />
+        <input type="text" placeholder={currentUser.email} />
         <button>Apply</button>
       </DataWrapper>
       <span>First name</span>
       <DataWrapper>
-        <input type="text" />
+        <input type="text" placeholder={currentUser.first_name} />
         <button>Apply</button>
       </DataWrapper>
       <span>Last name</span>
       <DataWrapper>
-        <input type="text" />
+        <input type="text" placeholder={currentUser.last_name} />
         <button>Apply</button>
       </DataWrapper>
       <span>Password</span>
