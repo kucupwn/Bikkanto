cb3aa2d35145f69c2c182bac0745b1fb96444379
Bence Gombkötő <kucukasd@gmail.com>
2026-04-08 20:13:55 +0200

    handle data change in profile

--- Frontend/src/pages/Profile.tsx
+++ Frontend/src/pages/Profile.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import type { User } from "../types/userTypes";
+import { useState } from "react";
 
 interface Props {
   currentUser: User;
@@ -17,22 +18,40 @@ const DataWrapper = styled.div`
 `;
 
 export function Profile({ currentUser }: Props) {
-  console.log(currentUser);
+  const [email, setEmail] = useState<string>(currentUser.email);
+  const [firstName, setFirstName] = useState<string>(currentUser.first_name);
+  const [lastName, setLastName] = useState<string>(currentUser.last_name);
+
   return (
     <ProfileContainer>
       <span>Email</span>
       <DataWrapper>
-        <input type="text" placeholder={currentUser.email} />
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder={currentUser.email}
+        />
         <button>Apply</button>
       </DataWrapper>
       <span>First name</span>
       <DataWrapper>
-        <input type="text" placeholder={currentUser.first_name} />
+        <input
+          type="text"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+          placeholder={currentUser.first_name}
+        />
         <button>Apply</button>
       </DataWrapper>
       <span>Last name</span>
       <DataWrapper>
-        <input type="text" placeholder={currentUser.last_name} />
+        <input
+          type="text"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+          placeholder={currentUser.last_name}
+        />
         <button>Apply</button>
       </DataWrapper>
       <span>Password</span>
