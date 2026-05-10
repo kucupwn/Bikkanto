1d91353975124768a18d2f1828ce5f0e92a35906
Bence Gombkötő <kucukasd@gmail.com>
2026-05-10 15:47:16 +0200

    simplify bg color logic of ribbon

--- Frontend/src/components/feedbackRibbon/FeedbackRibbon.tsx
+++ Frontend/src/components/feedbackRibbon/FeedbackRibbon.tsx
@@ -1,4 +1,4 @@
-import styled, { css, keyframes } from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 export type RibbonType = "success" | "error";
 
@@ -7,24 +7,16 @@ const slideIn = keyframes`
   to   { opacity: 1; transform: translateY(0); }
 `;
 
-const typeStyles: Record<RibbonType, ReturnType<typeof css>> = {
-  success: css`
-    background: #6ce961;
-  `,
-  error: css`
-    background: #e15d5d;
-  `,
-};
-
 const Ribbon = styled.div<{ $type: RibbonType }>`
-  display: flex;
   position: absolute;
   border-radius: 10px;
   padding: 10px;
   top: 10px;
   right: 25px;
   animation: ${slideIn} 0.25s ease;
-  ${({ $type }) => typeStyles[$type]}
+  background-color: ${({ $type }) => {
+    return $type === "success" ? "#6ce961" : "#e15d5d";
+  }};
 `;
 
 const Message = styled.span``;
