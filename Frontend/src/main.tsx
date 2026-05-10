9f92b2f404d7996f216b07bf5e313e59b8a95470
Bence Gombkötő <kucukasd@gmail.com>
2026-05-10 12:26:42 +0200

    wrap app into ribbon provider

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { App } from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import { RibbonProvider } from "./components/feedbackRibbon/RibbonProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <RibbonProvider>
+        <App />
+      </RibbonProvider>
     </BrowserRouter>
   </StrictMode>,
 );
