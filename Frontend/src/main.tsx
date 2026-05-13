0a2dc025c21ceb5d0b8237db5fc81d056991724a
Bence Gombkötő <kucukasd@gmail.com>
2026-05-13 17:25:16 +0200

    add theme provider to main.tsx

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -4,12 +4,15 @@ import "./index.css";
 import { App } from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { RibbonProvider } from "./components/feedbackRibbon/RibbonProvider.tsx";
+import { ThemeProvider } from "./components/ThemeProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <RibbonProvider>
-        <App />
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
       </RibbonProvider>
     </BrowserRouter>
   </StrictMode>,
