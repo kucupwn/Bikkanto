1941e30e395fe681ac2ed81fff84814b830ef953
Bence Gombkötő <kucukasd@gmail.com>
2026-05-14 18:14:07 +0200

    set proper theme color for modal boxes

--- Frontend/src/components/ModalBase.tsx
+++ Frontend/src/components/ModalBase.tsx
@@ -1,5 +1,7 @@
 import type { ReactNode } from "react";
 import styled from "styled-components";
+import type { Themes } from "../App";
+import { useTheme } from "./ThemeProvider";
 
 const Overlay = styled.div`
   position: fixed;
@@ -11,7 +13,7 @@ const Overlay = styled.div`
   z-index: 1000;
 `;
 
-const ModalBox = styled.div`
+const ModalBox = styled.div<{ $theme: Themes }>`
   background-color: white;
   padding: 1rem;
   border-radius: 12px;
@@ -20,6 +22,9 @@ const ModalBox = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  background-color: ${({ $theme }) => {
+    return $theme === "dark" ? "#151515" : "#ecebeb";
+  }};
 `;
 
 interface Props {
@@ -28,9 +33,13 @@ interface Props {
 }
 
 export function ModalBase({ onClose, children }: Props) {
+  const { theme } = useTheme();
+
   return (
     <Overlay onClick={onClose}>
-      <ModalBox onClick={(e) => e.stopPropagation()}>{children}</ModalBox>
+      <ModalBox onClick={(e) => e.stopPropagation()} $theme={theme}>
+        {children}
+      </ModalBox>
     </Overlay>
   );
 }
