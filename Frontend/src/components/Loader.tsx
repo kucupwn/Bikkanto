2393a90f8e917f83d024052ddb899e155cb3eee5
Bence Gombkötő <kucukasd@gmail.com>
2026-05-23 13:22:35 +0200

    create loader component

---
+++ Frontend/src/components/Loader.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Text = styled.h2`
+  justify-self: center;
+  align-self: center;
+`;
+
+export function Loader() {
+  return <Text>Loading...</Text>;
+}
