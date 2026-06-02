e82d46a437a3390df389e06f7c4e887c8d96be70
Bence Gombkötő <kucukasd@gmail.com>
2026-06-02 15:56:53 +0200

    add getHistoryInRange in statsmodal

--- Frontend/src/components/stats/StatsModal.tsx
+++ Frontend/src/components/stats/StatsModal.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { ModalBase } from "../ModalBase";
 import HistoryRangePicker from "./HistoryRangePicker";
+import { api } from "../../api/api";
 
 interface Props {
   isOpen: boolean;
@@ -15,10 +16,23 @@ export function StatsModal({ isOpen, onClose }: Props) {
     new Date(),
   ]);
 
+  async function getHistoryInRange() {
+    try {
+      const res = await api.get("/history/range", {
+        params: {
+          start_date: dateRange[0]?.toISOString().split("T")[0],
+          end_date: dateRange[1]?.toISOString().split("T")[0],
+        },
+      });
+      console.log(res.data);
+    } catch (err: any) {}
+  }
+
   return (
     <ModalBase onClose={onClose}>
       <h2>Statistics</h2>
       <HistoryRangePicker dateRange={dateRange} setDateRange={setDateRange} />
+      <button onClick={getHistoryInRange}>Get</button>
     </ModalBase>
   );
 }
