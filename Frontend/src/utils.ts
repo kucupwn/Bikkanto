68765db0de5e7e843bd62e8b141308639c8ba615
Bence Gombkötő <kucukasd@gmail.com>
2026-06-06 12:12:32 +0200

    add date formatting helper func

--- Frontend/src/utils.ts
+++ Frontend/src/utils.ts
@@ -1,3 +1,9 @@
 export function capitalize(text: string) {
   return text.charAt(0).toUpperCase() + text.slice(1);
 }
+
+export function formatDate(date: Date | null) {
+  if (!date) return null;
+
+  return date.toISOString().split("T")[0];
+}
